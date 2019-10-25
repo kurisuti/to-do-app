1fdcc6d2c675f856316614025a08753eb321cc3a
christie chang <kurisuti.cc@gmail.com>
2019-10-24 17:31:16 -0700

    added index.html and app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+
+function onReady(){
+  const ADD_TODO_FORM= document.getElementById("addToDoForm");
+  const NEW_TODO_TEXT= document.getElementById("newToDoText");
+  const TODO_LIST= document.getElementById("toDoList");
+
+  ADD_TODO_FORM.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    let title = NEW_TODO_TEXT.value;
+
+    let newLi = document.createElement("li");// <li></li>
+    let checkbox = document.createElement("input");
+    checkbox.type= "checkbox";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function(event){
+      TODO.LIST.removeChild(this.parentElement);
+    })
+
+    newLi.textContent = title;
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
+    TODO_LIST.appendChild(newLi);
+    NEW_TODO_TEXT.value = "";
+  });
+};
+
+window.onload = function(){
+  onReady();
+};
